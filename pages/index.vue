f6bdc72133c7947dcc56ab95d23f143bdee112e1
rafabusleme <raabusleme@uc.cl>
2021-04-02 18:36:55 -0300

    :tada: first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>IIC3103 - T01</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstState: 'Hola mundo',
+    }
+  },
+}
+</script>
+
+<style></style>
