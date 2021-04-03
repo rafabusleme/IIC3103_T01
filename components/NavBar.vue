fddfc2f05222c41193cd5b3e0a3b468f1ba4e05b
rafabusleme <raabusleme@uc.cl>
2021-04-02 21:33:49 -0300

    :rocket: create app layout

---
+++ components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nav-container">
+    <div class="items">
+      <nuxt-link to="/" class="item">Home</nuxt-link>
+      <nuxt-link to="/about" class="item">About</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.nav-container {
+  background-color: #369457;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+}
+.items {
+  display: flex;
+}
+.item {
+  padding: 10px 20px;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+}
+.item:hover {
+  background-color: #1f6032;
+}
+</style>
