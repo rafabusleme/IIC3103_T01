4bf4dece534eebfa3dbc6373234dd24571c28dfb
rafabusleme <raabusleme@uc.cl>
2021-04-03 17:07:12 -0300

    :fire: fix main endpoint

--- services/breakingBad/index.js
+++ services/breakingBad/index.js
@@ -1,6 +1,6 @@
 export const getEpisodesBySerie = async function (axios, serieName) {
   try {
-    const response = await axios.get(`/episodes?=${serieName}`)
+    const response = await axios.get(`/episodes?series=${serieName}`)
     return response.data
   } catch (err) {
     console.error(err)
